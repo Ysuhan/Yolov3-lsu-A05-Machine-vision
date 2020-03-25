20170f39a4cb0c3dc4c9c48320d356b1ceda0891
Ysuhan <2478628760@qq.com>
2020-03-25 09:00:54 +0800

    预测类型：默认图片预测

--- predict.py
+++ predict.py
@@ -30,21 +30,10 @@ def Img():
 
 
 def View():
-    # vid = cv2.VideoCapture(0)
-    # if not vid.isOpened():
-    #     raise IOError("Couldn't open webcam or video")
-    # while True:
-    #     return_value, frame = vid.read()
-    #     image = Image.fromarray(frame)
-    #
-    #     image = np.asarray(image)
-    #     cv2.namedWindow("result", cv2.WINDOW_NORMAL)
-    #     cv2.imshow("result", image)
-    #     if cv2.waitKey(1) == 27:
-    #         break
+
     yolo.detect_video("logs/1.mp4")
 
 if __name__ == '__main__':
-    # Img()  # 图片预测
-    View()  # 相机预测
+    Img()  # 图片预测
+    # View()  # 相机预测
     yolo.close_session()
